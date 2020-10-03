69ffc0000bf93b48e003346f69e395c7443b4098
Lehich <lehichh@gmail.com>
2020-10-03 08:18:29 +0300

    feat: implement min, max and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    let min = 0;
+    if (!(array === undefined || array.length === 0)) {
+        min = array[1];
+        array.forEach(element => {
+            min = element < min ? element : min;
+        });
+    }
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    let max = 0;
+    if (!(array === undefined || array.length === 0)) {
+        max = array[1];
+        array.forEach(element => {
+            max = element > max ? element : max;
+        });
+    }
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    let avg = 0;
+    if (!(array === undefined || array.length === 0)) {
+        array.forEach(element => {
+            avg += element;
+        });
+
+        avg /= array.length;
+    }
+    return avg;
 }
